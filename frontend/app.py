2e86668ef3a417aeb41225edd9e7cfe7793092d1
Jan Jarzebinski <janjarzebinski1@gmail.com>
2026-09-24 19:40:04 +0200

    Added login

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, session, request
+from flask import Flask, render_template, url_for, session, request, redirect
 import os, sys, sqlite3, re
 from pathlib import Path
 
@@ -13,6 +13,7 @@ from backend.database import db
 app = Flask(__name__)
 
 app.config['DATABASE'] = project_root / 'backend' / 'database' / 'users.db'
+app.config['SECRET_KEY'] = 'dev-secret-key-change-later'
 
 db.init_app(app)
 
@@ -47,15 +48,31 @@ def register():
             cur.execute("INSERT INTO user VALUES (NULL, ?, ?, ?)", (username, email, password))
             con.commit()
             msg = 'You have successfully registered!'
-        return render_template('auth/register.html', msg=msg)
+        return redirect(url_for('login'))
 
     return render_template('auth/register.html', msg=msg)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    msg='test_login'
+    msg=''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
 
+        con = sqlite3.connect(app.config['DATABASE'])
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        res = cur.execute('SELECT * FROM user WHERE username = ? AND password = ?', (username, password))
+        account = res.fetchone()
+
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            return render_template('base.html', msg='Logged in successfully!')
+        else:
+            msg = 'Incorrect login/password!'
     return render_template('auth/login.html', msg=msg)
 
 
